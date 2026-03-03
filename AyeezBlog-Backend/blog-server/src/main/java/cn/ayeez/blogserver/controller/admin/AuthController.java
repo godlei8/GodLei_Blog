4e27bbaa836bbac55253093ed4356beb1aac72fa
Ayeez757 <3406608593@qq.com>
2026-03-03 14:19:59 +0800

    添加了filter
    给管理端/admin路径添加了filter拦截，/admin/login登录放行

--- AyeezBlog-Backend/blog-server/src/main/java/cn/ayeez/blogserver/controller/admin/AuthController.java
+++ AyeezBlog-Backend/blog-server/src/main/java/cn/ayeez/blogserver/controller/admin/AuthController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @Slf4j
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/admin")
 public class AuthController {
 
     @Autowired
--- AyeezBlog-Backend/blog-server/src/main/java/cn/ayeez/blogserver/controller/admin/PostController.java
+++ AyeezBlog-Backend/blog-server/src/main/java/cn/ayeez/blogserver/controller/admin/PostController.java
@@ -1,8 +1,11 @@
 package cn.ayeez.blogserver.controller.admin;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * 管理文章的controller
  */
+@RequestMapping("/admin/post")
 public class PostController {
 
     //TODO 管理端增加文章接口待开发
